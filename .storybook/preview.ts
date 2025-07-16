644142e27f375f65bd04b4418fc8ed6275488459
Vulpesvulpes825 <vulpesvulpes825@gmail.com>
2025-07-15 18:35:00 -0700

    add init files

---
+++ .storybook/preview.ts
@@ -0,0 +1,16 @@
+import type { Preview } from '@storybook/react-webpack5'
+
+import "antd/dist/reset.css";
+
+const preview: Preview = {
+  parameters: {
+    controls: {
+      matchers: {
+       color: /(background|color)$/i,
+       date: /Date$/i,
+      },
+    },
+  },
+};
+
+export default preview;
