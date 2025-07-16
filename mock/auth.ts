5a0c64410612bc5ac5808756ddfe4b8d815dff1d
Vulpesvulpes825 <vulpesvulpes825@gmail.com>
2025-07-16 07:59:28 -0700

    test(mock-auth): return different jwt based on login username

--- mock/auth.ts
+++ mock/auth.ts
@@ -12,13 +12,30 @@ export default {
   'POST /api/auth/login': async (req: Request, res: Response) => {
     const { username, password } = req.body;
     await waitTime(1000);
-    if ((username === 'admin' || username === 'user') && password === 'ant.design') {
-      // Return a mock JWT token
+    if (username === 'admin' && password === 'ant.design') {
       res.send({
         status: 'ok',
         type: 'account',
-        currentAuthority: username === 'admin' ? 'admin' : 'user',
-        token: 'mock-jwt-token',
+        currentAuthority: 'hr',
+        token: 'mock-jwt-token-hr',
+      });
+      return;
+    }
+    if (username === 'user' && password === 'ant.design') {
+      res.send({
+        status: 'ok',
+        type: 'account',
+        currentAuthority: 'employee',
+        token: 'mock-jwt-token-employee',
+      });
+      return;
+    }
+    if (username === 'onboard' && password === 'ant.design') {
+      res.send({
+        status: 'ok',
+        type: 'account',
+        currentAuthority: 'employee-onboard',
+        token: 'mock-jwt-token-employee-onboard',
       });
       return;
     }
