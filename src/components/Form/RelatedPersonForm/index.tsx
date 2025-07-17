f72aca0697cb1a67ecc9e69ab0e9ed7d53385419
Vulpesvulpes825 <vulpesvulpes825@gmail.com>
2025-07-17 06:23:16 -0700

    feat(components): wrap RelatedPersonForm's form with a card

--- src/components/Form/RelatedPersonForm/index.tsx
+++ src/components/Form/RelatedPersonForm/index.tsx
@@ -1,7 +1,8 @@
+import { Card } from 'antd';
 import React from 'react';
-import {NameForm} from '@/components/Form/NameForm';
-import ContactForm from '@/components/Form/ContactForm';
 import AddressForm from '@/components/Form/AddressForm';
+import ContactForm from '@/components/Form/ContactForm';
+import { NameForm } from '@/components/Form/NameForm';
 
 export interface RelatedPersonFormProps {
   disabled?: boolean;
@@ -17,14 +18,20 @@ const RelatedPersonForm: React.FC<RelatedPersonFormProps> = ({
   formInstance,
 }) => (
   <>
-    {/* Name fields */}
-    <NameForm initialValues={undefined} />
-    {/* Contact fields */}
-    <ContactForm disabled={disabled} />
-    {/* Address fields */}
-    <AddressForm form={formInstance} minAddresses={minAddresses} maxAddresses={maxAddresses} />
+    <Card title="Name Information" style={{ marginBottom: 24 }}>
+      <NameForm initialValues={undefined} />
+    </Card>
+    <Card title="Contact Information" style={{ marginBottom: 24 }}>
+      <ContactForm disabled={disabled} />
+    </Card>
+    <Card title="Address Information">
+      <AddressForm
+        form={formInstance}
+        minAddresses={minAddresses}
+        maxAddresses={maxAddresses}
+      />
+    </Card>
   </>
 );
 
 export default RelatedPersonForm;
-
